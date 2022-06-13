43f0d707b88b969a9e259bc6cc586c967a787a8b
IsabellaTrajano <bellrdapct@gmail.com>
2022-06-12 22:47:30 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let age = 20
+
+if(age > 18){
+    console.log('você é adulto')
+} 
+
+
+let idade = 25
+let cond = 'humano'
+
+if(idade > 18 && cond == 'humano'){
+    console.log('você é um humano adulto')
+} 
+
+let aniversário = 'dezembro'
+
+if(aniversário == 'janeiro' || aniversário == 'dezembro'){
+    console.log('feliz aniversário')
+}
+
+
